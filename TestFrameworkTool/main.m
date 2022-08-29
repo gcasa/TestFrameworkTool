7c7284d40bb48ecf150572b6930a34f63c53daed
Gregory John Casamento <greg.casamento@gmail.com>
2022-08-28 23:16:21 -0400

    Initial Commit

---
+++ TestFrameworkTool/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestFrameworkTool
+//
+//  Created by Gregory Casamento on 8/28/22.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}

268a315fb944e0e3fe86b3dd5c471a551f733b9f
Gregory John Casamento <greg.casamento@gmail.com>
2022-08-28 23:18:48 -0400

    Add test framework tool

--- TestFrameworkTool/main.m
+++ TestFrameworkTool/main.m
@@ -6,11 +6,14 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <TestFramework/TestObject.h>
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
         NSLog(@"Hello, World!");
+        TestObject *obj = [[TestObject alloc] init];
+        [obj testMethod: nil];
     }
     return 0;
 }
